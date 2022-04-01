99f4364626f732b13aa868d4162bd1e9a5904f16
stevo222 <stephenostler@yahoo.com>
2022-04-01 10:03:13 -0600

    Player controlls beggining

---
+++ Snow Day/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 8.0f;
+    private Rigidbody playerRb;
+    private float zBound = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        ConstrainPlayer();
+
+    }
+
+    // moves the player 
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+    }
+
+    //boundraies
+    void ConstrainPlayer()
+    {
+        if (transform.position.z < -zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zBound);
+        }
+
+        if (transform.position.z > zBound)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zBound);
+        }
+
+    }
+
+}
